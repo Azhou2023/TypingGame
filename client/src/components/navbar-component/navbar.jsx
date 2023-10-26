0db0722d0d7ef274d0a4b67dbe301b67c5b492dd
Azhou2023 <azhou2023@francisparker.org>
2023-10-25 20:29:29 -0700

    hack school day 3

--- client/src/components/navbar-component/navbar.jsx
+++ client/src/components/navbar-component/navbar.jsx
@@ -1,18 +1,25 @@
-import styles from './Navbar.module.css'
+import styles from './navbar.module.css'
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        /* return a navbar with a top level div, and links to the homepage, Game page, and History page! */
-        /* Hint: the Next.js Link component may come in handy https://nextjs.org/docs/pages/api-reference/components/link */
-        <div id={styles.navbar}>
+        
+        <div className={styles.navbar}>
             <div className={styles.title}>
-                {/* Link to the home page here! */}
+                <Link href="/">Home</Link>
+              
             </div>
-
+            <div className={styles.nav_buttons}>
+            <Link href="/game">Add Sentence</Link>
+            <Link href="/game">Play Game</Link>
+            <Link href="/history">History</Link>
             {/* Link to other pages here! */}
+            </div>
 
         </div>
     )
 }
+
+export default Navbar;
 
 // Export the NavBar, and import it in pages/_app.jsx to render it on all pages of your website!
--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import '../components/navbar-component/navbar.jsx'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
